4d34236ee18164a6648ea18bb970b05eea745cdf
JEEM\Jonathan <jonathanedu@hotmail.es>
2022-11-25 13:09:26 -0500

    Creación de portfolio

---
+++ src/components/Knowledge.tsx
@@ -0,0 +1,59 @@
+import { v4 as uuidv4 } from "uuid";
+import { Language, Framework_DB, Others, Icarousel } from "./common/Common";
+import "./Knowledge.css";
+import KnowledgeCarousel from "./KnowledgeCarousel";
+
+const carousel: Icarousel[] = [
+  {
+    title: "Lenguajes",
+    images: [
+      Language.typescriptImg,
+      Language.cSharpImg,
+      Language.visualBasicImg,
+      Language.javaImg,
+      Language.cPlusPlusImg,
+      Language.pythonImg,
+    ],
+  },
+  {
+    title: "Frameworks / DB",
+    images: [
+      Framework_DB.reactImg,
+      Framework_DB.angularImg,
+      Framework_DB.djangoSoloImg,
+      Framework_DB.DB2Img,
+      Framework_DB.sqlImg,
+      Framework_DB.mysqlImg,
+      Framework_DB.potgressImg,
+    ],
+  },
+  {
+    title: "Otros",
+    images: [
+      Others.html_css_js_bootstrapImg,
+      Others.primeNgImg,
+      Others.jiraImg,
+      Others.gitHubImg,
+      Others.bitbucketImg,
+      Others.jenkinsImg,
+      Others.sonarqubeImg,
+      Others.bizagiImg,
+      Others.mqttImg,
+      Others.mosquittoImg,
+      Others.visualStudioCodeImg,
+      Others.visualStudioImg,
+    ],
+  },
+];
+
+function Knowledge() {
+  return (
+    <div className="knowledge">
+      {carousel.map((item) => {
+        return <KnowledgeCarousel key={uuidv4()} {...item} />;
+      })}
+    </div>
+  );
+}
+
+export default Knowledge;
